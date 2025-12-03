e8f8de2044637a817c84f171f046df01e5114dd5
Danny Hogan <dlhogan@uw.edu>
2025-12-03 15:55:06 -0800

    updated date merging

--- 01c_data_merging/gothic/merge_gothic_precipitation.py
+++ 01c_data_merging/gothic/merge_gothic_precipitation.py
@@ -12,18 +12,45 @@ DATA_PATH = '/storage/dlhogan/precipitation-rodeo/data/processed/'
 
 if __name__ == "__main__":
     # Load datasets from Gothic
-    billy_barr_ds = xr.open_dataset(f'{DATA_PATH}billy_barr/billy_barr_20211001-20230930_30min.nc')['precip'].to_dataset().sortby('time')
+    billy_barr_ds = xr.open_dataset(f'{DATA_PATH}billy_barr/billy_barr_20211001-20230930_30min.nc')[['precip','precip_bad_flag','precip_missing_flag',]].sortby('time')
+
+    # rename flags to qc_missing_billy_barr_precip and qc_bad_billy_barr_precip
+    billy_barr_ds = billy_barr_ds.rename({'precip_missing_flag':'qc_missing_billy_barr_precip',
+                                        'precip_bad_flag':'qc_bad_billy_barr_precip'})
     sail_ld_ds = xr.open_dataset(f'{DATA_PATH}SAIL/laser_disdrometer_gothic_processed_30min.nc')[['precip_accum_uncorrected','precip_accum_holyroyd',
-                                                                                                'precip_accum_brandes','precip_accum_heymsfield',]].sortby('time')
-    sail_pluvio_ds = xr.open_dataset(f'{DATA_PATH}SAIL/pluvio_30min.nc')['accum_nrt'].to_dataset().sortby('time')
+                                                                                                'precip_accum_brandes','precip_accum_heymsfield',
+                                                                                                'precip_missing_flag','precip_bad_flag',]].sortby('time')
+    # rename variables to qc_missing_sail_ld and qc_bad_sail_ld
+    sail_ld_ds = sail_ld_ds.rename({'precip_missing_flag':'qc_missing_sail_ld_uncorrected',
+                                    'precip_bad_flag':'qc_bad_sail_ld_uncorrected'})
+    sail_pluvio_ds = xr.open_dataset(f'{DATA_PATH}SAIL/pluvio_30min.nc')[['accum_nrt','pluvio_missing_flag','pluvio_bad_flag',]].sortby('time')
+    # rename variables to qc_missing_sail_pluvio and qc_bad_sail_pluvio
+    sail_pluvio_ds = sail_pluvio_ds.rename({'pluvio_missing_flag':'qc_missing_sail_pluvio',
+                                            'pluvio_bad_flag':'qc_bad_sail_pluvio'})
+    # load sail met dataset
     sail_met_ds = xr.open_dataset(f'{DATA_PATH}SAIL/met_30min.nc').sortby('time')
-    sail_squire_ds = xr.open_dataset(f'{DATA_PATH}SAIL/squire_30min.nc').sel(site='gothic').sortby('time').squeeze()
 
     # drop all vars in sail_met_ds not in process_sail_data.SAIL_PRECIPITATION_VARS['cumulative'] 
     met_prcp_vars = [var for var in process_sail_data.SAIL_PRECIPITATION_VARS['cumulative'] if var in sail_met_ds.data_vars]
-    squire_prcp_vars = [var for var in process_sail_data.SAIL_PRECIPITATION_VARS['cumulative'] if var in sail_squire_ds.data_vars]
-    sail_squire_ds = sail_squire_ds[squire_prcp_vars].drop_vars(['lat','lon','x','y','site'])
+    # add qc variables for sail_met_ds
+    met_prcp_vars += [f'{var}_missing_flag' for var in ['org_precip', 'pwd_precip', 'tbrg_precip']]
+    met_prcp_vars += [f'{var}_bad_flag' for var in ['org_precip', 'pwd_precip', 'tbrg_precip']]
     sail_met_ds = sail_met_ds[met_prcp_vars]
+    # rename sail met qc variables
+    sail_met_ds = sail_met_ds.rename({'org_precip_missing_flag':'qc_missing_sail_org',
+                                        'org_precip_bad_flag':'qc_bad_sail_org',
+                                        'pwd_precip_missing_flag':'qc_missing_sail_pwd',
+                                        'pwd_precip_bad_flag':'qc_bad_sail_pwd',
+                                        'tbrg_precip_missing_flag':'qc_missing_sail_tbg',
+                                        'tbrg_precip_bad_flag':'qc_bad_sail_tbg',})
+    # load squire dataset 
+    sail_squire_ds = xr.open_dataset(f'{DATA_PATH}SAIL/squire_30min.nc').sel(site='gothic').sortby('time').squeeze()
+    squire_prcp_vars = [var for var in process_sail_data.SAIL_PRECIPITATION_VARS['cumulative'] if var in sail_squire_ds.data_vars]
+    squire_prcp_vars += ["squire_missing_flag","squire_bad_flag"]
+    sail_squire_ds = sail_squire_ds[squire_prcp_vars].drop_vars(['lat','lon','x','y','site'])
+    # rename sail squire qc variables
+    sail_squire_ds = sail_squire_ds.rename({'squire_missing_flag':'qc_missing_sail_squire_m2009_1',
+                                            'squire_bad_flag':'qc_bad_sail_squire_m2009_1'})
 
     print('Merging datasets...')
     try:
@@ -33,6 +60,7 @@ if __name__ == "__main__":
         print(f'Error merging datasets: {e}')
 
     # rename the variables
+    # rename the variables
     variable_renames = {
         'precip': 'billy_barr_precip',
         'precip_accum_uncorrected': 'sail_ld_uncorrected',
@@ -40,15 +68,13 @@ if __name__ == "__main__":
         'precip_accum_brandes':'sail_ld_brandes',
         'precip_accum_heymsfield':'sail_ld_heymsfield',
         'accum_nrt':'sail_pluvio',
-        'pwd_cumul_rain':'sail_pwd_rain',
+        'pwd_precip_total':'sail_pwd',
         'tbrg_precip_total':'sail_tbg',
-        'tbrg_precip_total_corr':'sail_tbg_corr',
         'org_precip_accum':'sail_org',
-        'rain_rate_A_total':'sail_squire_rain',
-        'snow_rate_m2009_1_total':'sail_squire_snow_m2009_1',
-        'snow_rate_m2009_2_total':'sail_squire_snow_m2009_2',
-        'snow_rate_ws88diw_total':'sail_squire_snow_ws88diw',
-        'snow_rate_ws2012_total':'sail_squire_snow_ws2012'
+        'snow_rate_m2009_1_total':'sail_squire_m2009_1',
+        'snow_rate_m2009_2_total':'sail_squire_m2009_2',
+        'snow_rate_ws88diw_total':'sail_squire_ws88diw',
+        'snow_rate_ws2012_total':'sail_squire_ws2012'
     }
 
     gothic_combined_ds = gothic_combined_ds.rename(variable_renames)
@@ -64,9 +90,18 @@ if __name__ == "__main__":
             gothic_combined_ds[var] = gothic_combined_ds[var].where((gothic_combined_ds[var] <= MAX), np.nan)
             gothic_combined_ds[var] = gothic_combined_ds[var].where(gothic_combined_ds[var] >= MIN, 0)
     print('Bad data removed.')
+    # update all bad flags to 1 where data is nan
+    for var in gothic_combined_ds.data_vars:
+        if 'qc_bad' in var:
+            data_var = var.replace('qc_bad_', '')
+            gothic_combined_ds[var] = gothic_combined_ds[var].where(~gothic_combined_ds[data_var].isnull(), 1)
+    # convert all qc variables to int
+    for var in gothic_combined_ds.data_vars:    
+        if 'qc_' in var:
+            gothic_combined_ds[var] = gothic_combined_ds[var].astype(int)
 
     # Save the merged dataset
-    output_filepath = f'{DATA_PATH}final/gothic_precipitation_30min.nc'
+    output_filepath = f'{DATA_PATH}final/gothic_precipitation_30min_with_flags.nc'
     try:
         gothic_combined_ds.to_netcdf(output_filepath)
         print(f'Merged dataset saved to {output_filepath}')
--- 01c_data_merging/kettle_ponds/merge_kettle_ponds_precipitation.py
+++ 01c_data_merging/kettle_ponds/merge_kettle_ponds_precipitation.py
@@ -11,18 +11,46 @@ from utils import process_sail_data
 DATA_PATH = '/storage/dlhogan/precipitation-rodeo/data/processed/'
 
 if __name__ == "__main__":
-    # Load datasets from Kettle p\Ponds
-    billy_barr_ds = xr.open_dataset(f'{DATA_PATH}billy_barr/billy_barr_20211001-20230930_30min.nc')[['precip']]
+    # Load billy barr data
+    billy_barr_ds = xr.open_dataset(f'{DATA_PATH}billy_barr/billy_barr_20211001-20230930_30min.nc')[['precip','precip_bad_flag','precip_missing_flag',]].sortby('time')
+    # rename flags to qc_missing_billy_barr_precip and qc_bad_billy_barr_precip
+    billy_barr_ds = billy_barr_ds.rename({'precip_missing_flag':'qc_missing_billy_barr_precip',
+                                        'precip_bad_flag':'qc_bad_billy_barr_precip'})
+    
+    # splash pluvio data
     splash_lpdf_ds = xr.open_dataset(f'{DATA_PATH}SPLASH/lpdf_gauge_30min.nc')
-    splash_ld_ds = xr.open_dataset(f'{DATA_PATH}SPLASH/SPLASH_kp_laser_disdrometer_30min.nc')[['Amount']]
-    sos_ds = xr.open_dataset(f'{DATA_PATH}SOS/sos_ds_30min.nc')[['SWE_p1_c_max_accum','SWE_p2_c_max_accum',
-                                                                'SWE_p3_c_max_accum','SWE_p4_c_max_accum',]].diff(dim='time')
+    # rename qc_variables
+    splash_lpdf_ds = splash_lpdf_ds.rename({'qc_missing_precip':'qc_missing_splash_pluvio',
+                                            'qc_bad_precip':'qc_bad_splash_pluvio'})
+    # laser disdrometer data
+    splash_ld_ds = xr.open_dataset(f'{DATA_PATH}SPLASH/SPLASH_kp_laser_disdrometer_30min.nc')[['Amount', 'qc_missing_precip', 'qc_bad_precip',
+                                                                                               'precip_accum_holyroyd', 
+                                                                                               'precip_accum_brandes', 
+                                                                                               'precip_accum_heymsfield',
+                                                                                               'precip_type']]
+    # rename qc variables
+    splash_ld_ds = splash_ld_ds.rename({'qc_missing_precip':'qc_missing_splash_ld_uncorrected',
+                                        'qc_bad_precip':'qc_bad_splash_ld_uncorrected'})
+    # sos data
+    sos_ds = xr.open_dataset(f'{DATA_PATH}SOS/sos_ds_30min.nc')[['SWE_p1_c_max_accum', 'qc_SWE_p1_c_missing', 'qc_SWE_p1_c_bad',
+                                                                'SWE_p2_c_max_accum', 'qc_SWE_p2_c_missing', 'qc_SWE_p2_c_bad',
+                                                                'SWE_p3_c_max_accum', 'qc_SWE_p3_c_missing', 'qc_SWE_p3_c_bad',
+                                                                'SWE_p4_c_max_accum', 'qc_SWE_p4_c_missing', 'qc_SWE_p4_c_bad']].sortby('time')
+    # rename qc variable to remove _c
+    sos_ds = sos_ds.rename({var: var.replace('_c_', '_') for var in sos_ds.data_vars if 'qc_' in var})
     sos_ds = sos_ds.where(sos_ds>0, 0)
+
+    # sail squire data
     sail_squire_ds = xr.open_dataset(f'{DATA_PATH}SAIL/squire_30min.nc').sel(site='kettle_ponds').sortby('time').squeeze()
+    squire_prcp_vars = [var for var in process_sail_data.SAIL_PRECIPITATION_VARS['cumulative'] if var in sail_squire_ds.data_vars]
+    squire_prcp_vars += ["squire_missing_flag","squire_bad_flag"]
+    sail_squire_ds = sail_squire_ds[squire_prcp_vars].drop_vars(['lat','lon','x','y','site'])
+    # rename sail squire qc variables
+    sail_squire_ds = sail_squire_ds.rename({'squire_missing_flag':'qc_missing_sail_squire_m2009_1',
+                                            'squire_bad_flag':'qc_bad_sail_squire_m2009_1'})
     
     MIN = 0.05  # minimum measurable precipitation
     MAX = 28.44  # 100-year event over 30 minutes
-    
     # merge kettle ponds datasets
     print('Merging Kettle Ponds datasets...')
     try:
@@ -33,22 +61,24 @@ if __name__ == "__main__":
 
     variable_renames_kp = {
                             'precip': 'billy_barr_precip',
-                            'prcp':'splash_lpdf',
+                            'prcp':'splash_pluvio',
                             'Amount': 'splash_ld_uncorrected',
+                            'precip_accum_holyroyd' : 'splash_ld_holyroyd',
+                            'precip_accum_brandes':'splash_ld_brandes',
+                            'precip_accum_heymsfield':'splash_ld_heymsfield',
                             'SWE_p1_c_max_accum': "sos_swe_p1",
                             'SWE_p2_c_max_accum': "sos_swe_p2",
                             'SWE_p3_c_max_accum': "sos_swe_p3",
                             'SWE_p4_c_max_accum': "sos_swe_p4",
-                            'rain_rate_A_total':'sail_squire_rain',
-                            'snow_rate_m2009_1_total':'sail_squire_snow_m2009_1',
-                            'snow_rate_m2009_2_total':'sail_squire_snow_m2009_2',
-                            'snow_rate_ws88diw_total':'sail_squire_snow_ws88diw',
-                            'snow_rate_ws2012_total':'sail_squire_snow_ws2012'
+                            'snow_rate_m2009_1_total':'sail_squire_m2009_1',
+                            'snow_rate_m2009_2_total':'sail_squire_m2009_2',
+                            'snow_rate_ws88diw_total':'sail_squire_ws88diw',
+                            'snow_rate_ws2012_total':'sail_squire_ws2012'
                           }
 
     kettle_ponds_combined_ds = kettle_ponds_combined_ds.rename(variable_renames_kp)
     # drop the following
-    to_drop = ['snow_rate_ws88diw','snow_rate_m2009_1','snow_rate_m2009_2','snow_rate_ws2012',]
+    to_drop = ['snow_rate_ws88diw','snow_rate_m2009_1','snow_rate_m2009_2','snow_rate_ws2012','sail_squire_rain']
     kettle_ponds_combined_ds = kettle_ponds_combined_ds.drop_vars(to_drop, errors='ignore')
 
     # remove any obvious bad data: negative precipitation values or 30 minute precipitation > 28.44 mm (100-year event over 30 minutes)
@@ -56,6 +86,16 @@ if __name__ == "__main__":
         kettle_ponds_combined_ds[var] = kettle_ponds_combined_ds[var].where((kettle_ponds_combined_ds[var] <= MAX), np.nan)
         kettle_ponds_combined_ds[var] = kettle_ponds_combined_ds[var].where(kettle_ponds_combined_ds[var] >= MIN, 0)
     print('Maximum and minimum thresholds applied to Kettle Ponds data.')
+
+    # update all bad flags to 1 where data is nan
+    for var in kettle_ponds_combined_ds.data_vars:
+        if 'qc_bad' in var:
+            data_var = var.replace('qc_bad_', '')
+            kettle_ponds_combined_ds[var] = kettle_ponds_combined_ds[var].where(~kettle_ponds_combined_ds[data_var].isnull(), 1)
+    # convert all qc variables to int
+    for var in kettle_ponds_combined_ds.data_vars:    
+        if 'qc_' in var:
+            kettle_ponds_combined_ds[var] = kettle_ponds_combined_ds[var].astype(int)
 
     # Save the merged dataset
     output_filepath = f'{DATA_PATH}final/kettle_ponds_precipitation_30min.nc'
